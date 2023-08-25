55ac2f093ea43c5316bbcc9b12b2bccf3b72bae3
alby11 <alby11@duck.com>
2023-08-25 09:31:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center">
+Ranger installation prerun plugin for xxh
+</p>
+
+<p align="center">  
+If you like the idea click ⭐ on the repo and <a href="https://twitter.com/intent/tweet?text=Nice%20plugin%20for%20the%20xxh%20project!&url=https://github.com/alby11/xxh-plugin-prerun-ranger" target="_blank">tweet</a>.
+</p>
+
+## Install
+```shell
+xxh +I https://github.com/alby11/xxh-plugin-prerun-ranger
+```
+
+## Usage
+```shell
+xxh host
+# ...
+```
+
+## Known issues
+
+...
+
+## Credits
+
+This plugin was created with [xxh-prerun-plugin cookiecutter template](https://github.com/xxh/cookiecutter-xxh-plugin-prerun).
---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+main() {
+  #############
+  # <Example>
+	need_cmd curl
+	need_cmd grep
+	need_cmd cut
+	need_cmd xargs
+	need_cmd chmod
+  # </Example>
+  #############
+	build
+}
+
+build() {
+  CDIR="$(cd "$(dirname "$0")" && pwd)"
+  build_dir=$CDIR/build
+
+  while getopts A:K:q option
+  do
+    case "${option}"
+    in
+      q) QUIET=1;;
+      A) ARCH=${OPTARG};;
+      K) KERNEL=${OPTARG};;
+    esac
+  done
+
+  rm -rf $build_dir
+  mkdir -p $build_dir
+
+  cd $CDIR
+  cp *prerun.sh *pluginrc.* $build_dir/
+
+  cd $build_dir
+
+  # Install ranger on the remote host
+  if [ ! -d "${XXH_HOME}/.xxh/plugins/xxh-plugin-prerun-ranger/build/ranger" ]; then
+    git clone --depth 1 https://github.com/ranger/ranger.git ${XXH_HOME}/.xxh/plugins/xxh-plugin-prerun-ranger/build/ranger
+  fi
+}
+
+cmd_chk() {
+  >&2 echo Check "$1"
+	command -v "$1" >/dev/null 2>&1
+}
+
+need_cmd() {
+  if ! cmd_chk "$1"; then
+    error "need $1 (command not found)"
+    exit 1
+  fi
+}
+
+main "$@" || exit 1
