6f85a3fc8589d0ebfc4b1d8aab59170014e5b0be
alby11 <alby11@duck.com>
2023-08-25 11:30:42 +0200

    edit variables

--- prerun.sh
+++ prerun.sh
@@ -1,6 +1,19 @@
-CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#!/usr/bin/env bash
+
+# This script will be executed ON THE HOST when you connect to the host.
+# Put here your functions, environment variables, aliases and whatever you need.
 
 #############################
 # Example:
 #export PATH=$CURR_DIR:$PATH
 #############################
+
+CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+echo $CURR_DIR
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+plugin_name='xxh-plugin-prerun-zsh-ranger'
+
+export PATH=$CURR_DIR/build/ranger:$PATH
+# export BAT_THEME="GitHub"
+# alias cat='bat --paging=never'
+# fpath=($CURR_DIR/bat/autocomplete $fpath)
