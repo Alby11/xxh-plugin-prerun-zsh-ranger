4e5119c7f36b9ac00f9b46557259e9a0c3bcd522
alby11 <alby11@duck.com>
2023-08-25 11:39:00 +0200

    edit variables

--- prerun.sh
+++ prerun.sh
@@ -8,12 +8,13 @@
 #export PATH=$CURR_DIR:$PATH
 #############################
 
-CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
-echo $CURR_DIR
-CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 plugin_name='xxh-plugin-prerun-zsh-ranger'
 
-export PATH=$CURR_DIR/build/ranger:$PATH
+# CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+# CURR_DIR is plugin build dir
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PATH=$CURR_DIR/ranger:$PATH
 # export BAT_THEME="GitHub"
 # alias cat='bat --paging=never'
 # fpath=($CURR_DIR/bat/autocomplete $fpath)
