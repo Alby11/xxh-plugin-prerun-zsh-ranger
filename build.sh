aa50ca1634ce27a3fdfc64ecb9b166178b8e1332
alby11 <alby11@duck.com>
2023-08-25 11:08:25 +0200

    edit variables and logic

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ main() {
 build() {
   local plugin_name="xxh-plugin-prerun-zsh-ranger"
   # plugin_dir="$(cd "$(dirname "$0")" && pwd)"
-  echo "XXH_HOME is:" $XXH_HOME
-  local xxh_plugins_dir="${XXH_HOME}/.xxh/plugins/"
+  # echo "XXH_HOME is:" $XXH_HOME
+  local xxh_plugins_dir="$HOME/.xxh/plugins/"
   local plugin_dir="$xxh_plugins_dir/$plugin_name"
   local build_dir=$plugin_dir/build
   echo "BUILD DIR IS:" $build_dir
